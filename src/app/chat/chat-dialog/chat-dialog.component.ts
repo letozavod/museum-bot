59fe4c186628c3eebc2f8a9422967cf9a8c8602e
vanyaErohin <bulahoff7@gmail.com>
2019-10-22 14:38:52 +0500

    Added help

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -20,11 +20,18 @@ export class ChatDialogComponent implements OnInit {
     // appends to array after each new message is added to feedSource
     this.messages = this.chat.conversation.asObservable()
       .pipe(scan((acc, val) => acc.concat(val), []));
+
+    this.formValue = 'Привет!';
+    this.sendMessage();
   }
 
-  sendMessage() {
-    if (!this.formValue) { return false; }
+  sendMessage(msg = null) {
+    if (!this.formValue && !msg) { return false; }
+    if (msg) {
+        this.chat.converse(msg);
+    } else {
     this.chat.converse(this.formValue);
+    }
     this.formValue = '';
   }
 
